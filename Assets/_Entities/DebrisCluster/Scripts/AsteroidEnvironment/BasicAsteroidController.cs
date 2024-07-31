e123fb47efc7a34faecf370ab658ec8551142e15
bshap93 <bshap93@gmail.com>
2024-07-31 14:56:00 -0600

    Add Explosions to the asteroids

--- Assets/_Entities/DebrisCluster/Scripts/AsteroidEnvironment/BasicAsteroidController.cs
+++ Assets/_Entities/DebrisCluster/Scripts/AsteroidEnvironment/BasicAsteroidController.cs
@@ -1,31 +1,28 @@
 using System.Collections;
-using System.Collections.Generic;
-using PixelCrushers.DialogueSystem;
 using UnityEngine;
-using UnityEngine.Serialization;
+using PixelCrushers.DialogueSystem;
 
 public class BasicAsteroidController : MonoBehaviour
 {
-    [FormerlySerializedAs("_initialRotationForceVector")]
     public Vector3 initialRotationForceVector;
-    [FormerlySerializedAs("_initialMovementForceVector")]
-    public Vector3 initialMovementForceVector;
     public float RotationIntensity = 100;
-
+    public GameObject dustCloudPrefab;
+    public AudioClip dustCloudBurstSound;
     public float hitPoints = 100f;
-    [FormerlySerializedAs("_sphereColliderObject")] [FormerlySerializedAs("sphereCollider")]
     public SphereCollider sphereColliderObject;
+    public float dustCloudLifetime = 5f;
+    public float explosionForce = 10f;
+    public float explosionRadius = 5f;
 
-    private GameObject _parent;
-    // Start is called before the first frame update
+    private AudioSource _audioSource;
+    private Rigidbody _rigidbody;
+
     void Start()
     {
-        _parent = GameObject.Find("DebrisClusterStationary_01");
-        initialMovementForceVector = Vector3.zero;
         initialRotationForceVector = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-        // GetComponent<Rigidbody>().AddForce(_initialMovementForceVector * 1000, ForceMode.Impulse);
-        GetComponent<Rigidbody>().AddTorque(initialRotationForceVector * RotationIntensity, ForceMode.Impulse);
-
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddTorque(initialRotationForceVector * RotationIntensity, ForceMode.Impulse);
+        _audioSource = GetComponent<AudioSource>();
 
         CreateSphereCollider();
     }
@@ -35,37 +32,57 @@ public class BasicAsteroidController : MonoBehaviour
         hitPoints -= damage;
         if (hitPoints <= 0)
         {
-            Destroy(gameObject);
+            CreateDustCloudExplosion();
             IncrementQuestVariable("Asteroids.NumDestroyed", 1);
+            Destroy(gameObject);
+        }
+    }
+
+    void CreateDustCloudExplosion()
+    {
+        if (dustCloudPrefab != null)
+        {
+            GameObject dustCloudInstance = Instantiate(dustCloudPrefab, transform.position, Quaternion.identity);
+
+            // Play sound
+            if (_audioSource != null && dustCloudBurstSound != null)
+            {
+                _audioSource.PlayOneShot(dustCloudBurstSound, 1f);
+            }
+
+            // Add explosion force to the dust particles
+            ParticleSystem particleSystem = dustCloudInstance.GetComponent<ParticleSystem>();
+            if (particleSystem != null)
+            {
+                ParticleSystem.MainModule main = particleSystem.main;
+                main.startSpeed = explosionForce;
+                main.startLifetime = dustCloudLifetime;
+            }
+
+            // Destroy the dust cloud after the lifetime
+            Destroy(dustCloudInstance, dustCloudLifetime);
         }
     }
 
     void IncrementQuestVariable(string variableName, int incrementAmount)
     {
-        // Get the current value of the variable
         int currentValue = DialogueLua.GetVariable(variableName).asInt;
         Debug.Log($"Current value of {variableName}: {currentValue}");
 
-        // Increment the value
         int newValue = currentValue + incrementAmount;
-
-        // Set the new value
         DialogueLua.SetVariable(variableName, newValue);
 
-        // Verify the new value
         int verifyValue = DialogueLua.GetVariable(variableName).asInt;
         Debug.Log($"New value of {variableName}: {verifyValue}");
 
-        // Force update
         DialogueManager.Instance.SendUpdateTracker();
     }
 
     private void CreateSphereCollider()
     {
-        sphereColliderObject = new SphereCollider();
         sphereColliderObject = gameObject.AddComponent<SphereCollider>();
         sphereColliderObject.radius = 100f;
         sphereColliderObject.isTrigger = true;
-        sphereColliderObject.center = new Vector3(0, 0, 0);
+        sphereColliderObject.center = Vector3.zero;
     }
 }
