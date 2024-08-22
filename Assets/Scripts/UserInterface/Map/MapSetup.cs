19bd0df803d391cbfce92076d57884899afaa234
bshap93 <bshap93@gmail.com>
2024-08-22 12:57:26 -0600

    Add URP and graphing sharers

--- Assets/Scripts/UserInterface/Map/MapSetup.cs
+++ Assets/Scripts/UserInterface/Map/MapSetup.cs
@@ -4,14 +4,15 @@ namespace UserInterface.Map
 {
     public class MapSetup : MonoBehaviour
     {
-        public MapLocationManager locationManager;
+        MapLocationManager _locationManager;
 
         void Start()
         {
+            _locationManager = MapLocationManager.Instance;
             // Add locations (x and y should be between 0 and 1)
-            locationManager.AddLocationName("Hab Skiff", new Vector2(0.4f, 0.3f));
-            locationManager.AddLocationName("Fleet Headquarters", new Vector2(0.6f, 0.8f));
-            locationManager.AddLocationName("Separatists", new Vector2(0.8f, 0.4f));
+            _locationManager.AddLocationName("Starship Enoch", new Vector2(0.5f, 0.5f));
+            _locationManager.AddLocationName("", new Vector2(0.6f, 0.8f));
+            _locationManager.AddLocationName("Freehold Station", new Vector2(0.8f, 0.6f));
         }
     }
 }
---
+++ Assets/Scripts/Utilities/CelestialNameGenerator.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    public class CelestialNameGenerator : MonoBehaviour
+    {
+        // Prefixes for stars
+        private static readonly string[] StarPrefixes =
+            { "Alpha", "Beta", "Gamma", "Delta", "Epsilon", "Zeta", "Eta", "Theta", "Iota", "Kappa" };
+
+        // Suffixes for stars
+        private static readonly string[] StarSuffixes =
+            { "Centauri", "Cygni", "Draconis", "Eridani", "Leonis", "Orionis", "Persei", "Tauri", "Ursae", "Velorum" };
+
+        // Prefixes for planets
+        private static readonly string[] PlanetPrefixes =
+            { "New", "Old", "Great", "Lesser", "Upper", "Lower", "Inner", "Outer" };
+
+        // Suffixes for planets
+        private static readonly string[] PlanetSuffixes = { "world", "terra", "globe", "sphere", "orb" };
+
+        // Root words for planet names
+        private static readonly string[] PlanetRoots =
+        {
+            "Aether", "Chrom", "Dyn", "Eos", "Ferra", "Gaia", "Helio", "Io", "Jovia", "Krypto", "Luna", "Meso", "Nova",
+            "Ocea", "Plu", "Quantum", "Rego", "Sola", "Terra", "Umbra", "Vortex", "Xeno", "Yggdra", "Zephyr"
+        };
+
+        public static string GenerateStarName()
+        {
+            string prefix = StarPrefixes[Random.Range(0, StarPrefixes.Length)];
+            string suffix = StarSuffixes[Random.Range(0, StarSuffixes.Length)];
+            return $"{prefix} {suffix}";
+        }
+
+        public static string GeneratePlanetName()
+        {
+            // 50% chance of using a prefix
+            bool usePrefix = Random.value > 0.5f;
+            // 30% chance of using a suffix
+            bool useSuffix = Random.value > 0.7f;
+
+            string name = PlanetRoots[Random.Range(0, PlanetRoots.Length)];
+
+            if (usePrefix)
+            {
+                string prefix = PlanetPrefixes[Random.Range(0, PlanetPrefixes.Length)];
+                name = $"{prefix} {name}";
+            }
+
+            if (useSuffix)
+            {
+                string suffix = PlanetSuffixes[Random.Range(0, PlanetSuffixes.Length)];
+                name = $"{name}{suffix}";
+            }
+
+            return name;
+        }
+
+        public static string GenerateRandomName()
+        {
+            // Generate a completely random name
+            int syllables = Random.Range(2, 5);
+            string name = "";
+            string vowels = "aeiou";
+            string consonants = "bcdfghjklmnpqrstvwxyz";
+
+            for (int i = 0; i < syllables; i++)
+            {
+                name += consonants[Random.Range(0, consonants.Length)];
+                name += vowels[Random.Range(0, vowels.Length)];
+            }
+
+            return char.ToUpper(name[0]) + name.Substring(1);
+        }
+    }
+}
